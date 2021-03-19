20fb7dcf08922cae83a6723754d5b5bab7e216a8
Zubair Hasan Zubo <zubairhasan@staffasia.org>
2021-03-19 16:32:40 +0600

    CourseSingleHeader.js Fixed

--- pages/course/[id].js
+++ pages/course/[id].js
@@ -1,14 +1,14 @@
-import React from 'react';
-import CourseSingleHeader from '../components/coursesinglepage/CourseSingleHeader';
-import CourseDetails from '../components/coursesinglepage/CourseDetails';
+import React from "react";
+import CourseSingleHeader from "../../components/coursesinglepage/CourseSingleHeader";
+import CourseDetails from "../../components/coursesinglepage/CourseDetails";
 
 const coursesingle = () => {
-    return (
-        <div>
-            <CourseSingleHeader />
-            <CourseDetails />
-        </div>
-    );
+  return (
+    <div>
+      <CourseSingleHeader />
+      <CourseDetails />
+    </div>
+  );
 };
 
 export default coursesingle;
